f48dccf64c7c11ca4900594c5efd4f0340ec5493
John Mata <john.mata@epn.edu.ec>
2024-06-20 00:16:13 -0500

    Add CORS, Morgan, and Helmet middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 import routerUser from './routers/user.routes.js';
 import routerTanker from './routers/tanker.routes.js';
 import routerDriver from './routers/driver.routes.js';
@@ -7,8 +10,10 @@ import routerDriver from './routers/driver.routes.js';
 const app = express();
 dotenv.config();
 
-
 app.set("port" , process.env.PORT || 3000 );
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.get("/" , ( req , res ) => res.send("Server on"));
 app.use('/api/v1', routerUser);
